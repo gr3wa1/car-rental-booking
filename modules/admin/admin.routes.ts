27ae706fa25a7381cb4965ca7376c4e13f59b8bb
Jatin Sidhu <Jsidhu5542@conestogac.on.ca>
2026-04-23 19:01:28 -0400

    Done - admin.routes

---
+++ modules/admin/admin.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from "express";
+import {
+  acceptBookingAdmin,
+  declineBookingAdmin,
+  getAdminDashboard,
+  getAllBookingsAdmin,
+  getAllUsersAdmin,
+} from "./admin.controller";
+import { authMiddleware } from "../../middlewares/auth.middleware";
+import { roleMiddleware } from "../../middlewares/role.middleware";
+
+const router = Router();
+
+router.use(authMiddleware, roleMiddleware("admin"));
+
+router.get("/dashboard", getAdminDashboard);
+router.get("/users", getAllUsersAdmin);
+router.get("/bookings", getAllBookingsAdmin);
+router.patch("/bookings/:id/accept", acceptBookingAdmin);
+router.patch("/bookings/:id/decline", declineBookingAdmin);
+
+export default router;
